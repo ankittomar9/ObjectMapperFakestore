50bd2b64833ce22e6352f07c5b60d1e03645813b
Ankit <tomarankit78@gmail.com>
2025-03-01 17:13:11 +0530

    Object Mapper Learning Example

---
+++ src/test/java/com/company/objectmapperfakestore/ObjectMapperFakestoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.objectmapperfakestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObjectMapperFakestoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
